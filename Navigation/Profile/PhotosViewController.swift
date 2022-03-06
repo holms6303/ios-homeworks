a01924f9839f3a7f7ec4dfcaf3b1d7dc6d2863c6
holms6303 <emoboy108@mail.ru>
2022-03-06 18:09:09 +0300

    Modificated file (add cells to screen and formatting by mockup )

--- Navigation/Profile/PhotosViewController.swift
+++ Navigation/Profile/PhotosViewController.swift
@@ -12,12 +12,16 @@ class PhotosViewController: UIViewController {
     private lazy var layout: UICollectionViewFlowLayout = {
         let layout = UICollectionViewFlowLayout()
         layout.scrollDirection = .vertical
+        layout.minimumLineSpacing = 8
+        layout.minimumInteritemSpacing = 8
         return layout
     }()
     private lazy var collectionView: UICollectionView = {
         let collectionView = UICollectionView(frame: .zero, collectionViewLayout: self.layout)
+        collectionView.delegate = self
+        collectionView.dataSource = self   
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "DefaultCollectionCell")
         collectionView.translatesAutoresizingMaskIntoConstraints = false
-        collectionView.backgroundColor = .yellow
 
         return collectionView
     }()
@@ -25,6 +29,12 @@ class PhotosViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.setupView()
+        self.title = "Photo Gallery"
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: animated)
     }
 
     private func setupView() {
@@ -39,4 +49,21 @@ class PhotosViewController: UIViewController {
             topConstraint, bottomConstraint,leadingConstraint, trailingConstraint
         ])
     }
+}
+
+extension PhotosViewController: UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+//        количество ячеек в секции
+        return 150
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "DefaultCollectionCell", for: indexPath)
+        cell.backgroundColor = .black
+        return cell
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+           return UIEdgeInsets(top: 8, left: 8, bottom: 8, right: 8)
+        }
+    
 }
